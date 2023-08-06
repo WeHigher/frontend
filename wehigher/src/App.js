2d5cc215a95122d7c11b954670ee43020307c990
seungsimdang <oak20005@naver.com>
2023-08-07 02:16:17 +0900

    Feat: 간단한 챗봇 기능 추가

--- wehigher/src/App.js
+++ wehigher/src/App.js
@@ -1,9 +1,36 @@
+import './App.css';
+
+function sendMessage() {
+  const userInput = document.getElementById('user-input-text').value;
+  const chatLog = document.getElementById('chat-log');
+  const userMessage = document.createElement('div');
+  userMessage.textContent = 'You: ' + userInput;
+  chatLog.appendChild(userMessage);
+
+  const chatGptResponse = 'ChatGPT: ' + '안녕하세요! 저는 ChatGPT입니다.';
+  const chatGptMessage = document.createElement('div');
+  chatGptMessage.textContent = chatGptResponse;
+  chatLog.appendChild(chatGptMessage);
+
+  document.getElementById('user-input-text').value = '';
+}
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="chat-container">
+      <div className="chat-log" id="chat-log">
+        {/* <!-- 채팅 로그 표시 --> */}
+      </div>
+      <div className="user-input">
+        <input
+          type="text"
+          id="user-input-text"
+          placeholder="문장을 입력하세요..."
+        ></input>
+        <button onClick={sendMessage}>전송</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
